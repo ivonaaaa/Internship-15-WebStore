9b68e412b788639d6d077240b2c0101446c66c5f
ivonaaaa <ivonaa.e@gmail.com>
2025-02-26 00:31:17 +0100

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  // u return bi trebale ic rute i valjda navigacija ako ona treba bit svugdi?
+  return <></>;
+}
+
+export default App;
